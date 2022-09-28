d2d18ef46a324a61ad8ed396abdd110e9557a7d6
ruanSignori <cleosignori@gmail.com>
2022-09-28 12:39:45 -0300

    change background color navbar

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -2,6 +2,7 @@ export const darkTheme = {
   "--bg-color_100": "#18181B",
   "--bg-color_200": "#1F2937",
   "--bg-color_300": "#374151",
+  "--bg-color-transparent": "rgba(36,36,36,.7)",
   "--text-color_100": "#F9FAFB",
   "--text-color_200": "#E5E7EB"
 };
--- src/themes/light.js
+++ src/themes/light.js
@@ -2,6 +2,7 @@ export const lightTheme = {
   "--bg-color_100": "#F9FAFB",
   "--bg-color_200": "#EEEEEE",
   "--bg-color_300": "#E5E7EB",
+  "--bg-color-transparent": "rgba(255,255,255,.7)",
   "--text-color_100": "#374151",
   "--text-color_200": "#1F2937"
 };
