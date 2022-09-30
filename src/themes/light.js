6303a0b36dec684a2020fcb6542ddce4703550c4
ruanSignori <cleosignori@gmail.com>
2022-09-30 09:01:38 -0300

    change color

--- src/themes/light.js
+++ src/themes/light.js
@@ -1,5 +1,5 @@
 export const lightTheme = {
-  "--bg-color_100": "#F9FAFB",
+  "--bg-color_100": "#f1f5f9",
   "--bg-color_200": "#EEEEEE",
   "--bg-color_300": "#E5E7EB",
   "--bg-color-transparent": "rgba(255,255,255,.7)",
