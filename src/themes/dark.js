7bb5b3b3626fa34501d21e1d74143440a57ab1af
ruanSignori <cleosignori@gmail.com>
2023-02-21 12:47:32 -0300

    feature: add new color in dark.js and light.js

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -1,8 +1,9 @@
 export const darkTheme = {
+  "--bg-color_0": "#131314",
   "--bg-color_100": "#18181B",
   "--bg-color_200": "#1F2937",
   "--bg-color_300": "#374151",
   "--bg-color-transparent": "rgba(36,36,36,.7)",
   "--text-color_100": "#F9FAFB",
-  "--text-color_200": "#E5E7EB"
+  "--text-color_200": "#E5E7EB",
 };
--- src/themes/light.js
+++ src/themes/light.js
@@ -1,8 +1,9 @@
 export const lightTheme = {
+  "--bg-color_0": "#EFF1F2",
   "--bg-color_100": "#F9F9F9",
   "--bg-color_200": "#EEEEEE",
   "--bg-color_300": "#E5E7EB",
   "--bg-color-transparent": "rgba(255,255,255,.7)",
   "--text-color_100": "#374151",
-  "--text-color_200": "#1F2937"
+  "--text-color_200": "#1F2937",
 };
