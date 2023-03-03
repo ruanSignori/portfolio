41ca2946e82a91e480265f2a90d7bc4f8addf0ac
ruanSignori <cleosignori@gmail.com>
2023-03-03 07:23:16 -0300

    feat: insert new color

--- src/themes/light.js
+++ src/themes/light.js
@@ -3,6 +3,7 @@ export const lightTheme = {
   "--bg-color_100": "#F9F9F9",
   "--bg-color_200": "#EEEEEE",
   "--bg-color_300": "#E5E7EB",
+  "--bg-color_400": "#D3D4D6",
   "--bg-color-transparent": "rgba(255,255,255,.7)",
   "--text-color_100": "#374151",
   "--text-color_200": "#1F2937",
