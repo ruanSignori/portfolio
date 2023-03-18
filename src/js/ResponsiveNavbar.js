ce0e0224010a24e5f4d59aa3c8493c84657aafe0
ruanSignori <cleosignori@gmail.com>
2023-03-18 20:53:34 -0300

    fix: changed responsiveness of navbar and contact section

--- src/js/ResponsiveNavbar.js
+++ src/js/ResponsiveNavbar.js
@@ -1,47 +1,54 @@
 const buttonOpenMenu = document.querySelector("#home > nav > button.menu");
-const bodyElement = document.querySelector('body');
+const bodyElement = document.querySelector("body");
 const modal = document.querySelector("#home > nav > div.content-navbar");
-const linkRef = document.querySelectorAll("#home > nav > div.content-navbar > .item-navbar");
+const linkRef = document.querySelectorAll(
+  "#home > nav > div.content-navbar > .item-navbar"
+);
+const switchThemeElement = document.querySelector(
+  "#home > nav > .context-switch"
+);
 let modalIsOpen = false;
 
 const showModal = () => {
-  modal.classList.add('onVisibility');
-  buttonOpenMenu.classList.add('active');
+  modal.classList.add("onVisibility");
+  buttonOpenMenu.classList.add("active");
+  modal.appendChild(switchThemeElement);
 };
 
 const hiddenModal = () => {
-  modal.classList.remove('onVisibility');
-  buttonOpenMenu.classList.remove('active');
+  modal.classList.remove("onVisibility");
+  buttonOpenMenu.classList.remove("active");
 };
 
 // Para cada link que for clicado no modal
 // Irá remover as propriedades de overflow do body, e esconder o modal
 linkRef.forEach((element) => {
-  element.addEventListener('click', () => {
+  element.addEventListener("click", () => {
     hiddenModal();
-    bodyElement.style.removeProperty('overflow');
+    bodyElement.style.removeProperty("overflow");
     modalIsOpen = false;
   });
 });
 
-// Se o tamanho da janela for maior que 800px e o menu estiver aberto
+// Se o tamanho da janela for maior que 1.200px e o menu estiver aberto
 // ele será removido do DOM
-window.addEventListener('resize', () => {
-  const checkMinWidth = window.matchMedia("(min-width: 800px)").matches;
-  const checkIfTheElementHasAClass = modal.classList.contains('onVisibility');
-  
+window.addEventListener("resize", () => {
+  const checkMinWidth = window.matchMedia("(min-width: 1200px)").matches;
+  const checkIfTheElementHasAClass = modal.classList.contains("onVisibility");
+
   if (checkMinWidth && checkIfTheElementHasAClass) {
-    modal.classList.remove('onVisibility');
-  };
+    hiddenModal();
+    bodyElement.style.removeProperty("overflow");
+  }
 });
 
 // Funcionalidade de ao clicar no botão, abrir o modal
-buttonOpenMenu.addEventListener('click', () => {
-  modalIsOpen ? modalIsOpen = false : modalIsOpen = true;
+buttonOpenMenu.addEventListener("click", () => {
+  modalIsOpen ? (modalIsOpen = false) : (modalIsOpen = true);
 
   modalIsOpen ? showModal() : hiddenModal();
-  
-  modal.classList.contains('onVisibility') ? 
-    bodyElement.style.overflow = 'hidden' :
-    bodyElement.style.removeProperty('overflow');
+
+  modal.classList.contains("onVisibility")
+    ? (bodyElement.style.overflow = "hidden")
+    : bodyElement.style.removeProperty("overflow");
 });
