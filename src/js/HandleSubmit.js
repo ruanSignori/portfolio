d936f2c9fa4deb80bf43aae2b382eec7784d0c23
ruanSignori <cleosignori@gmail.com>
2023-03-31 20:58:08 -0300

    style: add comment in code

--- src/js/HandleSubmit.js
+++ src/js/HandleSubmit.js
@@ -9,8 +9,10 @@ const handleSubmit = async (event) => {
   event.preventDefault();
   buttonSubmit.setAttribute("disabled", "true");
 
+  // Pegar os dados do formulário
   const data = new FormData(event.target);
 
+  // Se algum dos input estiver vazio, o formulário e reconfigurado, irá cair no tratamento de erro.
   for (const value of data) {
     if (value[1].trim(" ") === "") {
       form.requestSubmit();
@@ -26,18 +28,21 @@ const handleSubmit = async (event) => {
     },
   })
     .then((response) => {
+      
       if (response.ok) {
         form.reset();
         buttonSubmit.removeAttribute("disabled");
         return toast.ok("Sucesso", "E-mail enviado!");
       }
 
+      // Se acontecer algum erro que não está previsto, vai cair no tratamento de erro.
       response.json().then((data) => {
         if (Object.hasOwn(data, "errors")) {
           const messageError = data["errors"].map((error) => error["message"]);
 
           return toast.error("Error", messageError);
         }
+        form.requestSubmit();
         toast.error("Error", "Não foi possível enviar o seu formulário");
       });
 
