d117462687c3f9d9c4d20f4c7f4b3c2986914e50
ruan <cleosignori@gmail.com>
2024-03-15 23:56:46 -0300

    refacotr: changed variable of themes light/dark

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -1,10 +1,6 @@
 export const darkTheme = {
-  "--bg-color_0": "#131314",
-  "--bg-color_100": "#18181B",
-  "--bg-color_200": "#1F2937",
-  "--bg-color_300": "#374151",
-  "--bg-color_400": "#363E4B",
-  "--bg-color-transparent": "rgba(36,36,36,.7)",
-  "--text-color_100": "#F9FAFB",
-  "--text-color_200": "#E5E7EB",
+  "--bg-primary": "#1A1A1A",
+  "--bg-secondary": "#2F2F2F",
+  "--text-primary": "#E1E1E1",
+  "--text-secondary": "#979797",
 };
--- src/themes/light.js
+++ src/themes/light.js
@@ -1,10 +1,6 @@
 export const lightTheme = {
-  "--bg-color_0": "#EFF1F2",
-  "--bg-color_100": "#F9F9F9",
-  "--bg-color_200": "#EEEEEE",
-  "--bg-color_300": "#E5E7EB",
-  "--bg-color_400": "#D3D4D6",
-  "--bg-color-transparent": "rgba(255,255,255,.7)",
-  "--text-color_100": "#374151",
-  "--text-color_200": "#1F2937",
+  "--bg-primary": "#F9FAFB",
+  "--bg-secondary": "#D9DADC",
+  "--text-primary": "#1A1A1A",
+  "--text-secondary": "#2F2F2F",
 };
