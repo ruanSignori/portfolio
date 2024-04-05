459e88b0e6769ef9ddecf9d912b97165d00ae9cb
ruan <cleosignori@gmail.com>
2024-04-05 19:00:23 -0300

    feat: infinite scroll

--- src/js/Slider.js
+++ src/js/Slider.js
@@ -1,44 +1,48 @@
-const container = document.querySelectorAll(".card");
-const buttonLeft = document.getElementById("left");
-const buttonRight = document.getElementById("right");
+// const container = document.querySelectorAll(".card");
+// const buttonLeft = document.getElementById("left");
+// const buttonRight = document.getElementById("right");
 
-buttonLeft.addEventListener("click", () => {
-  toPrevious();
-});
+// buttonLeft.addEventListener("click", () => {
+//   toPrevious();
+// });
 
-buttonRight.addEventListener("click", () => {
-  toNext();
-});
+// buttonRight.addEventListener("click", () => {
+//   toNext();
+// });
 
-let current = 0;
+// let current = 0;
 
-let prev = container.length;
-let next = 1;
+// let prev = container.length;
+// let next = 1;
 
-const toPrevious = () => {
-  current > 0 ? toSlide(current - 1) : toSlide(container.length - 1);
-};
+// const toPrevious = () => {
+//   current > 0 ? toSlide(current - 1) : toSlide(container.length - 1);
+// };
 
-const toNext = () => {
-  current < container.length - 1 ? toSlide(current + 1) : toSlide(0);
-};
+// const toNext = () => {
+//   current < container.length - 1 ? toSlide(current + 1) : toSlide(0);
+// };
 
-const toSlide = (number) => {
-  current = number;
-  prev = current - 1;
-  next = current + 1;
+// const toSlide = (number) => {
+//   current = number;
+//   prev = current - 1;
+//   next = current + 1;
 
-  for (let i = 0; i < container.length; i++) {
-    container[i].classList.remove("active");
-    container[i].classList.remove("prev");
-    container[i].classList.remove("next");
-  }
+//   for (let i = 0; i < container.length; i++) {
+//     container[i].classList.remove("active");
+//     container[i].classList.remove("prev");
+//     container[i].classList.remove("next");
+//   }
 
-  if (next === container.length) next = 0;
+//   if (next === container.length) next = 0;
 
-  if (prev === -1) prev = container.length - 1;
+//   if (prev === -1) prev = container.length - 1;
 
-  container[current].classList.add("active");
-  container[prev].classList.add("prev");
-  container[next].classList.add("next");
-};
+//   container[current].classList.add("active");
+//   container[prev].classList.add("prev");
+//   container[next].classList.add("next");
+// };
+
+
+const copy = document.querySelector('.infinite-scroll ul').cloneNode(true);
+document.querySelector('.infinite-scroll').appendChild(copy)
